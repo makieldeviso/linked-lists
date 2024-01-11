41c815b0a8bd764346c4f76941390e906ea1c5e0
Fred Mark Baldeviso <fredmark.baldeviso@gmail.com>
2024-01-12 02:51:55 +0800

    Adds script/ more methods for LinkedLists

--- index.js
+++ index.js
@@ -12,54 +12,134 @@ class LinkedList {
     append (value) {
 
         if (this.size() === 0)  {
-            this['head'] = new Node(value);
+            this['HEAD'] = new Node(value);
 
         } else if (this.size() > 0) {
             // Create new Node;
             const newNode = new Node(value);
 
             if (this.size() === 1) {
-                this['head'].nextNode = newNode;
-                this['tail'] = newNode;
+                this['HEAD'].nextNode = newNode;
+                this['TAIL'] = newNode;
 
             } else {
                 // Reassign values for current tail
-                const currentTail = this['tail'];
+                const currentTail = this['TAIL'];
                 currentTail.nextNode = newNode;
 
                 // Add previous tail to the linked list
                 this[currentTail.value] = currentTail;
 
                 // Reassign tail of linked list to new Node
-                this['tail'] = newNode;
+                this['TAIL'] = newNode;
             }
         } 
 
     }   
 
     prepend(value) {
-        const currentHead = this['head'];
+        const currentHead = this['HEAD'];
         this[currentHead.value] = currentHead;
         
-        this['head'] = new Node(value);
-        this['head'].nextNode = currentHead;
+        this['HEAD'] = new Node(value);
+        this['HEAD'].nextNode = currentHead;
     }
 
     size() {
-        const countSize = function (head) {
-            let count = 0;
-            let current = head;
+        // const countSize = function (head) {
+        //     let count = 0;
+        //     let current = head;
         
-            if (!head) return 0
+        //     if (!head) return 0
 
-            while (current !== null) {
-                count ++;
-                current = current.nextNode;
-            }
+        //     while (current !== null) {
+        //         count ++;
+        //         current = current.nextNode;
+        //     }
 
-            return count;
+        //     return count;
+        // }
+
+        const countSize = function (head, count ) {
+            if (head === undefined) return 0
+
+            if (head === null) return count
+            
+            return countSize(head.nextNode, count + 1);
         }
-        return countSize(this.head);
+
+        return countSize(this['HEAD'], 0);
+    }
+
+    head () {
+        return this['HEAD'];
+    }
+
+    tail () {
+        return this['TAIL'];
+    }
+
+    at (index) {
+
+        // const getNodeAt = function (head, reqIndex) {
+        //     let count = 0
+        //     let current = head;
+            
+        //     if (!head) return null
+
+        //     while (current !== null) {
+        //         console.log(count)
+        //         if (count === reqIndex) return current
+        //         count ++
+        //         current = current.nextNode;
+        //     }
+
+        //     return null
+        // }
+
+        const getNodeAt = function (head, reqIndex) {
+            if (!head) return null
+
+            if (reqIndex === 0) return head
+
+            return getNodeAt(head.nextNode, reqIndex - 1);
+        }
+
+        return getNodeAt(this['HEAD'], index);
+    }
+
+    pop () {
+        const secondLast = this.at(this.size() - 2);
+        secondLast.nextNode = null;
+        delete this[secondLast.value];
+
+        this['TAIL'] = secondLast;
+    }
+
+    contains (value) {
+
+        const checkValue = function (head, reqValue) {
+            if (!head) return false;
+
+            if (head.value === reqValue) return true;
+
+            return checkValue(head.nextNode, reqValue);
+        }
+
+        return checkValue(this['HEAD'], value);
+    }
+
+    find (value) {
+
+        const getIndex = function (head, reqValue, index) {
+            if (!head) return null;
+
+            if (head.value === reqValue) return index;
+
+            return getIndex(head.nextNode, reqValue, index + 1);
+        }
+
+        return getIndex(this['HEAD'], value, 0);
     }
 }
 
@@ -69,7 +149,10 @@ newList.append('B');
 newList.append('C');
 newList.prepend('S');
 
-console.log(newList.size())
+console.log(newList);
+console.log(newList.contains('F'));
+console.log(newList.find('F'));
+
 
 
 
